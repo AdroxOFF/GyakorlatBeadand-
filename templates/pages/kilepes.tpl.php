9869e180a3ff285ba81f05b7e22c55beafe57070
AdroxOFF <theszabipresents2@gmail.com>
2026-05-03 18:44:35 +0200

    Enhance logout page with success message and link

--- templates/pages/kilepes.tpl.php
+++ templates/pages/kilepes.tpl.php
@@ -1,2 +1,9 @@
-<h1>Kilépett:</h1>
-<?= $data['csn']." ".$data['un']." (".$data['login'].")" ?>
+<h2>👋 Sikeres kilépés</h2>
+<p>
+    <?php if ($kilep_nev) { ?>
+        <strong><?= htmlspecialchars($kilep_nev) ?></strong> sikeresen kijelentkezett.
+    <?php } else { ?>
+        Sikeresen kijelentkezett.
+    <?php } ?>
+</p>
+<a href="." class="gomb gomb-primary" style="margin-top:15px; display:inline-block;">🏠 Vissza a főoldalra</a>
