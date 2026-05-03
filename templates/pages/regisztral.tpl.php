d18bda370577f859b83bb089c254d07c4fda78b5
AdroxOFF <theszabipresents2@gmail.com>
2026-05-03 21:26:08 +0200

    Implement registration result feedback in template
    Added registration result message display with conditional navigation.

--- templates/pages/regisztral.tpl.php
+++ templates/pages/regisztral.tpl.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Regisztráció</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <?php if(isset($uzenet)) { ?>
-            <h1><?= $uzenet ?></h1>
-            <?php if($ujra) { ?>
-                <a href="belepes">Próbálja újra!</a>
-            <?php } ?>
-        <?php } ?>
-    </body>  
-</html>
+<h2>📋 Regisztráció eredménye</h2>
+<?php if (isset($reg_uzenet)) { ?>
+    <div class="<?= $reg_ujra ? 'uzenet-hiba' : 'uzenet-ok' ?>">
+        <?= htmlspecialchars($reg_uzenet) ?>
+    </div>
+    <?php if ($reg_ujra) { ?>
+        <a href="belepes" class="gomb gomb-secondary" style="margin-top:12px; display:inline-block;">
+            ↩️ Vissza a belépéshez
+        </a>
+    <?php } else { ?>
+        <p style="margin-top:12px;">Most már <a href="belepes">be tud lépni</a> az oldalra.</p>
+    <?php } ?>
+<?php } ?>
